2fb94eba9b27e35d466abffd56a025f5a296bf39
acritelli <critellia@gmail.com>
2015-05-15 14:40:04 -0400

    Removing site.yml

--- roles/wordpress/files/scripts/add_wordpress_fail2ban.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo -e '[wordpress]\nenabled = true\nfilter = wordpress\nlogpath = /var/log/messages\nport = http, https\nmaxretry = 3\naction = iptables-allports[name=wordpress, protocol=all]\nbantime = 600' >> /etc/fail2ban/jail.conf
