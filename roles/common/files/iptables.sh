17ba8ad746662e320ac2cd58bc141697fb27db79
acritelli <critellia@gmail.com>
2015-05-14 20:11:59 -0400

    Updating iptables script to allow ICMP

--- roles/common/files/iptables.sh
+++ roles/common/files/iptables.sh
@@ -2,6 +2,7 @@
 
 iptables -P INPUT DROP
 iptables -F
+iptables -A INPUT -p icmp -j ACCEPT
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 
