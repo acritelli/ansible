6a32ea45fe71d5b25359372233339ea48694d006
acritelli <critellia@gmail.com>
2015-05-21 18:51:15 -0400

    Wordpress filter should be in jail.local not .conf

--- roles/wordpress/files/scripts/add_wordpress_fail2ban.sh
+++ roles/wordpress/files/scripts/add_wordpress_fail2ban.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo -e '[wordpress]\nenabled = true\nfilter = wordpress\nlogpath = /var/log/messages\nport = http, https\nmaxretry = 3\naction = iptables-allports[name=wordpress, protocol=all]\nbantime = 600' >> /etc/fail2ban/jail.conf
+echo -e '[wordpress]\nenabled = true\nfilter = wordpress\nlogpath = /var/log/messages\nport = http, https\nmaxretry = 3\naction = iptables-allports[name=wordpress, protocol=all]\nbantime = 600' >> /etc/fail2ban/jail.local
